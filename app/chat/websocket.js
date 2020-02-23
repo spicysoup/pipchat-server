d356b6b22fd0e4fa69a20e5f963abbbf79c8b1ef
Zhuang Xu <not-real@example.com>
2020-02-24 07:07:11 +1100

    Relay 1-on-1 messages to peer

--- app/chat/websocket.js
+++ app/chat/websocket.js
@@ -1,16 +1,36 @@
 module.exports = (server) => {
-  const { printObject } = require('../utils');
+  const {printObject} = require('../utils');
   const io = require('socket.io')(server);
+  const store = require('./store');
+
   io.on('connection', socket => {
-    io.emit('welcome', {message: 'Hello'});
+        console.log('New connection');
+        io.emit('welcome', {message: 'Hello'});
 
-    socket.on('chat/start', (data) => {
-      console.log(data);
-      socket.emit('chat/invited', {message: data});
-    });
+        socket.on('chat/start', (data) => {
+          console.log(data);
+          socket.emit('chat/invited', {message: data});
+        });
 
-    socket.on('chat/message', (data) => {
-      printObject(data);
-    });
-  });
+        socket.on('chat/message', (data) => {
+          printObject(data);
+
+          // A peer registers itself
+          if ('self' in data && !('peer' in data)) {
+            const {self: id} = data;
+            if (id in store.users) {
+              store.sockets[id] = socket;
+              console.log('New peer registered');
+              printObject(store.users);
+            }
+          }
+
+          const {self, peer, message} = data;
+          if (self && peer && message) {
+            console.log(`Relaying message to ${peer}`);
+            store.sockets[peer].emit('chat/message', {self, message});
+          }
+        });
+      },
+  );
 };
