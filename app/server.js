bda46cb79bd9565595051077236f9e5cf25ed0a4
Zhuang Xu <not-real@example.com>
2020-02-26 12:41:33 +1100

    Go back to HTTP from HTTPS

--- app/server.js
+++ app/server.js
@@ -2,14 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const fs = require('fs');
-
-const privateKey = fs.readFileSync('ssl-cert/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('ssl-cert/fullchain.pem', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-const https = require('https');
-
 const express = require("express");
 const app = express();
 
@@ -19,9 +11,5 @@ app.use(cors());
 app.use('/api', require('./chat/router'));
 
 const port = process.env.PORT || 3001;
-const server = https.createServer(credentials, app);
+const server = app.listen(port, () => console.log(`API listening on ${port}.`));
 require('./chat/websocket')(server);
-
-server.listen(port, () => console.log(`API listening on ${port}.`));
-
-
