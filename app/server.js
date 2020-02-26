72cd66970d6bfc9f451b7c2a0c449877fc071ade
Zhuang Xu <not-real@example.com>
2020-02-26 11:56:23 +1100

    Start using HTTPS

--- app/server.js
+++ app/server.js
@@ -2,6 +2,14 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('ssl-cert/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('ssl-cert/fullchain.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+const https = require('https');
+
 const express = require("express");
 const app = express();
 
@@ -11,6 +19,9 @@ app.use(cors());
 app.use('/api', require('./chat/router'));
 
 const port = process.env.PORT || 3001;
-const server = app.listen(port, () => console.log(`API listening on ${port}.`));
+const server = https.createServer(credentials, app);
 require('./chat/websocket')(server);
+
+server.listen(port, () => console.log(`API listening on ${port}.`));
+
 
