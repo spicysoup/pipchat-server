686054bbb1d07e4e0ec93cd1582585dfce55c8d3
Zhuang Xu <not-real@example.com>
2020-02-26 14:32:34 +1100

    Use HTTP or HTTPS depending on the environment

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
+
+const port = process.env.PORT || 3001;
 
 const express = require("express");
 const app = express();
@@ -10,6 +10,21 @@ app.use(cors());
 
 app.use('/api', require('./chat/router'));
 
-const port = process.env.PORT || 3001;
-const server = app.listen(port, () => console.log(`API listening on ${port}.`));
-require('./chat/websocket')(server);
+const fs = require('fs');
+
+let server;
+
+if (process.env.NODE_ENV === 'production') {
+  const privateKey = fs.readFileSync('ssl-cert/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('ssl-cert/fullchain.pem', 'utf8');
+
+  const credentials = {key: privateKey, cert: certificate};
+  const https = require('https');
+  server = https.createServer(credentials, app);
+  require('./chat/websocket')(server);
+  server.listen(port, () => console.log(`API listening on ${port}.`));
+} else {
+  require('./chat/websocket')(server);
+  server = app.listen(port, () => console.log(`Local API listening on ${port}.`));
+}
+
